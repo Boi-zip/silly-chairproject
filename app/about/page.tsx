627ed2c01e22c5c55a326a69b4689c21182415e6
Boi-zip <boirpm@outlook.com>
2025-09-29 08:05:14 +0200

    Added Github link

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -2,6 +2,8 @@ import Navbar from "@/components/Navbar"
 import Image from 'next/image'
 import "./style.css"
 import silly from "./pictures/silly.jpg"
+import github from "./pictures/github-mark-white.png"
+import Link from "next/link"
 export default function about(){
     return(
     <div>
@@ -10,9 +12,12 @@ export default function about(){
         <p className="textabout">
             Not sure what you want to know about me.
             <br/>
-            But this is just a silly site project, where only products that you can view are chairs, and 1 table
+            But this is just a silly site project, where only products that you can buy are chairs, and 1 table
         </p>
-        <Image className="img" src={silly} alt="silly" width={400} height={400}/>
+        <p className="textabout">
+            <Link href={"https://github.com/Boi-zip/silly-chairproject"}><Image src={github} alt="Github logo" width={45} height={45}/></Link>
+        </p>
+        <Image className="img" src={silly} alt="silly" width={375} height={375}/>
     </div>
     )
 }
