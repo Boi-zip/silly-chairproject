1e1c3c8005939498858d5592f1e6af51843a3326
Boi.zip <boirpm@outlook.com>
2026-02-10 16:20:32 +0100

    Improved styling & added few chairs to the main page to attract

---
+++ components/SellingItem_home.jsx
@@ -0,0 +1,22 @@
+"use client"
+import "./sellitem.css"
+import "animate.css"
+import Image from 'next/image'
+const SellingItemH = ({ text, price, img, desc}) => {
+  //Product card from https://codepen.io/sam_garcia2/pen/PozpPRy
+    return(
+
+            <div className="card">
+  <div className="imgBox">
+    <Image src={img} width={300} height={300} alt="chair" className="mouse"/>
+  </div>
+
+  <div className="contentBox">
+    <p className="font-semibold font-stretch-expanded text-2xl">{text}</p>
+    <p className="price">€{price}</p>
+    <p className="desc">{desc}</p>
+   </div>
+</div>
+    )
+}
+export default SellingItemH
